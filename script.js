56a4f86d56a60df72e385776c4031a60c08b4432
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-04 16:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+
+function selectColor(){
+    const colorSelect=document.getElementById('colorSelect')
+
+    const selectedOption=colorSelect.options[colorSelect.selectedIndex]
+
+    const selectedColor=selectedOption.value;
+
+    colorSelect.remove(colorSelect.selectedIndex);
+}
